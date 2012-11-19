80a376624a687c67794ea5bbb87fa616f322e6e4
Karan Khiani <karan-91@outlook.com>
2012-11-18 20:34:36 -0500

    Commit on late Sunday evening while trying to fix page reload problem (still unresolved)

--- js/BukketFull.js
+++ js/BukketFull.js
@@ -4,7 +4,7 @@
 
 //Function that lists all the existing items from localStorage
 function listItems(){
-	$('#bucket-item-list').append('');
+	//$('<li><a href="view.htm" data-transition="flip"><img src="img/mask.png" alt="list thumbnail" style="padding: 0.2em;">Will this work</a></li>').insertAfter($('#bucket-item-list-incomplete'));
 }
 
 //Function that runs when the user is adding an item to the bucket list
@@ -29,6 +29,7 @@ $("#confirm-add").live('click',function() {
 });
 
 //Function that gives the user random suggestions for bucket list items to add
-function randomSuggestion(){
-	var randomItems = new Array("Travel to Argentina", "");
-}
+$("#get-random").live('click',function() {
+	var randomItems = new Array("Travel to Argentina", "Eat African cuisine", "Hike the Grand Canyon", "Climb Mount Everest", "Dive the Great Barrier Reef", "Visit the Taj Mahal", "Throw a coin in the Trevi Fountain");
+	$('#bucket-text').val(randomItems[Math.floor(Math.random()*randomItems.length)]);
+});
